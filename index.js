2e4caa249b9fc2d57609ac7d2f685b7e154f1aac
Kelvin <vokemalosh32@gmail.com>
2022-06-06 23:19:32 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+// Write your code here 
+const num1=31;
+const num2=2;
+const multiply =num1*num2;
+//math.random
+const random =Math.floor(Math.random() * 10)+1;
+//modulus
+const num3 =9;
+const num4 =5;
+const mod=num3 % num4;
+//max
+const max = Math.max(1,3,5,6,12,14,20)
